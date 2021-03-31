35cff6b02879ebb031316dc40f7bcb785c5b8ccb
CodeZhifty <juulstausholmoscar@gmail.com>
2021-04-01 01:42:16 +0200

    Zia settings

--- projects/zia/login.js
+++ projects/zia/login.js
@@ -28,4 +28,14 @@ firebase.auth().onAuthStateChanged(function(user) {
     } else {
       // No user is signed in.
     }
-  });
+});
+
+password.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("login").click();
+    }
+});
