d19cf6945d2560f2e998c3f9829d2f7246228dd7
CodeZhifty <juulstausholmoscar@gmail.com>
2021-05-09 23:34:21 +0200

    Game adder functionallity

--- projects/browserlauncher/script.js
+++ projects/browserlauncher/script.js
@@ -1,3 +1,21 @@
+function download() {
+    const gameName = document.getElementById("gameName").value.toLowerCase().replace(/ /g, "");
+    const gamePath = document.getElementById("gamePath").value.replace(/\\/g, "\\\\");
+
+    const text = '@echo off\nset gameName=' + gameName + '\n(\necho Windows Registry Editor Version 5.00\necho.\necho [HKEY_CLASSES_ROOT\\%gameName%]\necho "URL Protocol"=""\necho [HKEY_CLASSES_ROOT\\%gameName%\\shell]\necho [HKEY_CLASSES_ROOT\\%gameName%\\shell\\open]\necho [HKEY_CLASSES_ROOT\\%gameName%\\shell\\open\\command]\necho @="' + gamePath + '"\n) > %gameName%.reg\nregedit /s %cd%\\%gameName%.reg'
+
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', gameName + ".bat");
+ 
+    element.style.display = 'none';
+    document.body.appendChild(element);
+ 
+    element.click();
+ 
+    document.body.removeChild(element);
+}
+
 function downloadPopup(){
     
 }
