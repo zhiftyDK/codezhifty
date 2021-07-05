cb006b42ab8bc9e7bf6b2e74906c7e422561b307
CodeZhifty <juulstausholmoscar@gmail.com>
2021-07-06 01:02:16 +0200

    added md5 script hashservice

--- projects/apis/hashservice.js
+++ projects/apis/hashservice.js
@@ -1,3 +1,8 @@
+//Add Md5
+let md5 = document.createElement("script");
+md5.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/blueimp-md5/2.18.0/js/md5.js");
+document.body.appendChild(md5);
+
 //Hashfunction
 function hash(input){
     const salt = Math.random().toString(36).substring(2).slice(0,5);
