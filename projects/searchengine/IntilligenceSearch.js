8565b76f2db2459921d87ce350ba4beb1574dae2
CodeZhifty <juulstausholmoscar@gmail.com>
2021-08-13 12:54:01 +0200

    Save

---
+++ projects/searchengine/IntilligenceSearch.js
@@ -0,0 +1,69 @@
+if(document.getElementById("Isearch")) {
+    //Isearch styling
+    const Isearch = document.getElementById("Isearch");
+    Isearch.style.textAlign = "center";
+
+    //Divs
+    const searchFieldDiv = document.createElement("div");
+    searchFieldDiv.id = "searchFieldDiv";
+    document.getElementById("Isearch").appendChild(searchFieldDiv);
+
+    const br = document.createElement("br");
+    document.getElementById("Isearch").appendChild(br);
+    
+    const responseFieldDiv = document.createElement("div");
+    responseFieldDiv.id = "responseFieldDiv";
+    responseFieldDiv.style.display = "flex";
+    responseFieldDiv.style.justifyContent = "center";
+    document.getElementById("Isearch").appendChild(responseFieldDiv);
+    
+    //Input searchfield
+    const input = document.createElement("input");
+    input.id = "searchField";
+    input.type = "text";
+    input.placeholder = "Search here";
+    input.style.borderRadius = "10px";
+    input.style.padding = "10px";
+    input.style.width = "50vw";
+    input.style.marginTop = "20vh";
+    input.style.marginBottom = "10vh";
+    document.getElementById("searchFieldDiv").appendChild(input);
+    
+    //Error Message Field
+    const errorMessage = document.createElement("h3");
+    errorMessage.id = "errorMessage";
+    errorMessage.style.display = "none";
+    document.getElementById("responseFieldDiv").appendChild(errorMessage);
+    
+    //Search
+    document.getElementById("searchField").addEventListener("keydown", function (){
+        if(document.getElementById("searchObject")) {
+            document.querySelectorAll("#searchObject").forEach(searchObject => {
+                searchObject.remove();
+            });
+        }
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "search.json");
+        xhr.responseType = "json";
+        xhr.onload = function(){
+            const data = xhr.response;
+            for(let i = 0; i < data.length; i++) {
+                if(data[i].keyword.includes(document.getElementById("searchField").value.toLowerCase())) {
+                    const searchObject = document.createElement("div");
+                    searchObject.id = "searchObject"
+                    searchObject.style.height = "100px";
+                    searchObject.style.width = "10vw";
+                    searchObject.style.backgroundColor = "black";
+                    searchObject.style.color = "white";
+                    searchObject.innerHTML = data[i].header;
+                    document.getElementById("responseFieldDiv").appendChild(searchObject);
+                }
+                else {
+                    document.getElementById("errorMessage").innerHTML = "There is nothing to see here!"
+                }
+            }
+        }
+        xhr.send();
+    })
+    document.body.onload(document.getElementById)
+}
